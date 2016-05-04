324c1b99ab9cc908a0d2117055d2c355a8d64d24
Wenting Ye <Wenting_Ye@Outlook.com>
2016-05-04 17:51:36 +0800

    fixed untracked files

--- Travel-Simulation-System I/Build/Intermediates/Travel-Simulation-System I.build/Debug/Travel-Simulation-System I.build/Objects-normal/x86_64/TrafficNet-2BAED56D54FFB93.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/yewenting/GitHub/Travel-Simulation-System/Travel-Simulation-System\ I/Travel-Simulation-System/TrafficNet.cpp \
-  /Users/yewenting/GitHub/Travel-Simulation-System/Travel-Simulation-System\ I/Travel-Simulation-System/TrafficNet.hpp
--- Travel-Simulation-System I/Build/Intermediates/Travel-Simulation-System I.build/Debug/Travel-Simulation-System I.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/yewenting/GitHub/Travel-Simulation-System/Travel-Simulation-System\ I/Travel-Simulation-System/main.cpp
